0ea292ecfc7f58fdfe4640e91df3f945416ec116
karim iarkane <karimiarkane6@gmail.com>
2023-11-26 14:10:21 -0800

    testing commit

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -5,8 +5,8 @@ import Image3 from '../Home/enlarge_myimage.jpg'
 
 
 const AboutMe = () => {
-  let formation = "i studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbzi studied 2 years at esi and then i moved into bbz"
-  let experience = "i built some websites like e commerce and portfolio website ..."
+  let formation = "I embarked on my educational journey with a strong passion for computer science, initially completing two years of focused study in high school. Recognizing the importance of advancing my knowledge and skills, I transitioned to the University of USTHB, where I enrolled as a second-year computer science engineering student. . The challenges and opportunities presented at the university level have been instrumental in shaping my understanding of the field, fostering a commitment to continuous learning and growth. My academic experience has equipped me with a solid foundation in computer science, and I am enthusiastic about applying my knowledge to real-world scenarios and contributing meaningfully to the ever-evolving technology landscape."
+  let experience = "igit comm built some websites like e commerce and portfolio website ..."
   let skills = "i know html css js and react c java github .."
   let karim = <div>karim hada an</div>
 
